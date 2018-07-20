0887fe32d3eee8d7b1f3166659034ec7ec06a38c
andrewprayle <andrew.prayle@nottingham.ac.uk>
2018-07-20 16:13:43 +0100

    Update the functions with new fucntions for graphs and a standard BA plot

--- ba_example.r
+++ ba_example.r
@@ -4,7 +4,6 @@ source("ba_functions.r")
 
 data1 <- read.csv("ba_2007_dataset.csv", stringsAsFactors = F, header = T)
 data1$diff <- data1$diff <- data1$RV - data1$IC
-data1$Subj. <- as.factor(data1$Subj.)
 
 plot(RV ~ IC, data = data1,
      pch = as.character(data1$Subj.), col = "white")
@@ -13,6 +12,11 @@ text(x = data1$IC, y = data1$RV, labels = data1$Subj., adj = NULL,
      pos = NULL, offset = 0.5, vfont = NULL,
      cex = 1, col = NULL, font = NULL)
 
-results_table <- get_ba_result(data1$RV, data1$IC, data1$Subj., plotit = T, verbose = F)
+results_table <- get_ba_result(data1$RV, data1$IC, data1$Subj., plotit = T, verbose = T)
 require(knitr)
 kable(results_table)
+kable(standard_ba(data1$RV, data1$IC, plotit = T, verbose = T))
+
+plot_sd(data1$RV, data1$IC, data1$Subj., plotit = T, verbose = T)
+plot_subject_averages(data1$RV, data1$IC, data1$Subj., verbose = T)
+
--- ba_functions.r
+++ ba_functions.r
@@ -18,14 +18,8 @@ get_anova_table <- function(x, g, verbose = F){
     
     fdata <- as.data.frame(cbind(x, g))
     if (verbose) print(table(fdata$g))
-    group_table <- fdata %>% group_by(g) %>% 
-        summarize(group_mean = mean(x),
-                  m = length(x))
+    group_table <- get_group_table(x, g, verbose = verbose)
     group_table$A_i <- group_table$group_mean - mean(fdata$x)
-    
-    if (verbose) print(group_table)
-    
-    
     
     ########################################################
     # calculate the df
@@ -69,12 +63,15 @@ get_group_table <- function(x, g, verbose = F){
   fdata <- as.data.frame(cbind(x, g))
   group_table <- fdata %>% group_by(g) %>% 
     summarize(group_mean = mean(x),
-              m = length(x))
+              m = length(x),
+              group_sd = sd(x))
   group_table$A_i <- group_table$group_mean - mean(fdata$x)
   group_table$m_2 <- group_table$m^2
   if(verbose) print(group_table)
   return(group_table)
 }
+
+
 
 get_ba_result <- function(a, b, g, verbose = F, plotit = T){
   # master function, loads the data, calls the functions
@@ -93,8 +90,8 @@ get_ba_result <- function(a, b, g, verbose = F, plotit = T){
   if (plotit) abline(h = 0)
   
   ## get the tables
-  anova_table <- get_anova_table(fdata$diff, fdata$g)
-  group_data <- get_group_table(fdata$diff, fdata$g)
+  anova_table <- get_anova_table(fdata$diff, fdata$g, verbose = verbose)
+  group_data <- get_group_table(fdata$diff, fdata$g, verbose = verbose)
   
   var_within_subject <- anova_table$MS[2]
   var_between_subject <- anova_table$MS[1] - anova_table$MS[2]
@@ -142,4 +139,65 @@ get_ba_result <- function(a, b, g, verbose = F, plotit = T){
   
 }
 
+plot_sd <- function(a, b, g, verbose = F, plotit = T){
+  fdata <- as.data.frame(cbind(a, b, g))
+  
+  fdata$diff <- fdata$a - fdata$b
+  
+  group_data <- get_group_table(fdata$diff, fdata$g)
+  group_data$size <- 1 / max(group_data$m) * group_data$m
+  
+  # do the ba plot
+  if (plotit)  plot(group_sd ~ group_mean, data = group_data,
+                    col = "black",
+                    xlab = "subject_mean_of_means",
+                    ylab = "subject_sd_difference",
+                    cex = group_data$size * 2)
+  if (plotit) abline(h = 0)
+}
 
+plot_subject_averages <- function(a, b, g, verbose = F, plotit = T){
+  fdata <- as.data.frame(cbind(a, b, g))
+  fdata$x <- fdata$a - fdata$b
+  
+  group_data <- fdata %>% group_by(g) %>% 
+    summarize(group_mean_diff  = mean(x),
+              m                = length(x),
+              group_sd         = sd(x),
+              group_mean_means = mean((a + b)/2)
+              )
+  if (verbose) print(group_data)
+  
+  group_data$size <- 1 / max(group_data$m) * group_data$m
+  
+  if (plotit)  plot(group_mean_diff ~ group_mean_means, data = group_data,
+                    col = "black",
+                    xlab = "subject_mean_of_means",
+                    ylab = "subject_mean_difference",
+                    cex = group_data$size * 2)
+  if (plotit) abline(h = 0)
+}
+
+standard_ba <- function(a, b, verbose = F, plotit = T){
+  fdata <- as.data.frame(cbind(a, b))
+  fdata$diff <- fdata$a - fdata$b
+  fdata$mean <- (fdata$a + fdata$b)/2
+  
+  plot(diff ~ mean, data = fdata,
+       xlab = "mean",
+       ylab = "diff")
+  
+  bias <- mean(fdata$diff)
+  upper <- bias + 1.96 * sd(fdata$diff)
+  lower <- bias - 1.96 * sd(fdata$diff)
+  results_table <- as.data.frame(matrix(data = NA, ncol = 2, nrow = 3))
+  colnames(results_table) <- c("Parameter", "Result")
+  results_table[1, 1] <- "Estimated bias"
+  results_table[2, 1] <- "upper 95% limit of agreement"
+  results_table[3, 1] <- "lower 95% limit of agreement "
+  results_table[1, 2] <- bias
+  results_table[2, 2] <- upper
+  results_table[3, 2] <- lower
+  
+  return(results_table)
+}
